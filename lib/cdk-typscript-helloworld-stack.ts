b881fd4fee5aecfcd11bd0a2a14ce1a66490a710
Ben Halverson <benhalverson33@gmail.com>
2022-06-16 23:22:25 -0700

    added endpoint with counter

--- lib/cdk-typscript-helloworld-stack.ts
+++ lib/cdk-typscript-helloworld-stack.ts
@@ -1,12 +1,32 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
+
 
 export class CdkTypscriptHelloworldStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    //define the lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,  //execution environment
+      code: lambda.Code.fromAsset('lambda'), //code from the lambda directory
+      handler: 'hello.handler' //file is "hello" and function is "handler"
+    });
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    //define an API Gateway REST API backed by our "hello" lambda function
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
+
+    new apigw.LambdaRestApi(this, 'EndpointWithCounter', {
+      handler: helloWithCounter.handler
+    });
   }
 }
